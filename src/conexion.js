fddf0444888a2501b0495eb50a15dfc2150b3388
unknown <richeti1998@gmail.com>
2020-10-11 19:59:30 -0500

    cc

--- src/conexion.js
+++ src/conexion.js
@@ -1,14 +1,2 @@
-const mysql = require('mysql');
 
-const mysqlConexion = mysql.createConnection({
-    host: 'birvxmqrxnbzpdvm0ogz-mysql.services.clever-cloud.com',
-    user: 'uysv9t18xsovpgmn',
-    password: 'lqXoWZythDxqtkIJbcOp',
-    database: 'birvxmqrxnbzpdvm0ogz'
-})
 
-mysqlConexion.connect(function (){
-    console.log('La BD esta conectada');
-});
-
-module.exports = mysqlConexion;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,13 @@
 const express = require('express')
+const mysql = require('mysql');
 const app = express();
+
+const mysqlConexion = mysql.createConnection({
+    host: 'birvxmqrxnbzpdvm0ogz-mysql.services.clever-cloud.com',
+    database: 'birvxmqrxnbzpdvm0ogz',
+    user: 'uysv9t18xsovpgmn',
+    password: 'lqXoWZythDxqtkIJbcOp'
+})
 
 // setings
 app.set('port', process.env.PORT || 3000);
@@ -9,6 +17,16 @@ app.use(express.json());
 
 // rutas
 app.use(require('./rutas/categorias'));
+
+mysqlConexion.connect(function (err){
+    if (err){
+        console.log('Error');
+    }else{
+        console.log('La BD esta conectada');
+    }
+});
+
+module.exports = mysqlConexion;
 
 // Start Servidor
 app.listen(app.get('port'), () => {
